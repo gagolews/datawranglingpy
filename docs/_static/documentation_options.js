0b29d9c0f79ae79d64a7d613c580f9acdc232acf
gagolews <m.gagolewski@gmail.com>
2022-10-28 12:54:54 +1100

    html metadata; numeric reference style

--- docs/_static/documentation_options.js
+++ docs/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: 'v1.0.2',
+    VERSION: 'v1.0.2.9001',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
