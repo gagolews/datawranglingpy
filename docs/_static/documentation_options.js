4b79411d247e38d7589527ec4bc72085821128f9
gagolews <m.gagolewski@gmail.com>
2023-01-09 15:42:13 +1100

    update 2023 + key packages

--- docs/_static/documentation_options.js
+++ docs/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: 'v1.0.2.9002',
+    VERSION: 'v1.0.2.9003',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
