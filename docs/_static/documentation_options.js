0aeff1e257046f7ba304162e9d53d44254a15a50
gagolews <m.gagolewski@gmail.com>
2023-01-31 15:02:34 +1100

    df dtypes

--- docs/_static/documentation_options.js
+++ docs/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: 'v1.0.2.9003',
+    VERSION: 'v1.0.2.9004',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
